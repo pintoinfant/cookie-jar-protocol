9308731b9ee1359bd608518d784a9d3986f7c42a
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-08-10 15:44:39 +0530

    push everything

--- web/src/app/frames/profile/route.tsx
+++ web/src/app/frames/profile/route.tsx
@@ -1,6 +1,6 @@
 import { Button } from "frames.js/next";
 import { frames } from "../frames";
-import jar from "../../../public/logo/money-jar.png";
+
 import { getProfileData } from "@/lib/getProfileData";
 
 const handleRequest = frames(async (ctx) => {
@@ -9,45 +9,42 @@ const handleRequest = frames(async (ctx) => {
 
   return {
     image: (
-      <div tw="bg-black flex w-full h-full flex-col p-10 " style={{ gap: 80 }}>
+      <div tw="bg-black flex w-full h-full flex-col p-10 " style={{ gap: 100 }}>
         <div tw="flex justify-between items-center">
           <img
-            src="http://localhost:3000/logo/header.png"
+            src="http://localhost:3000/logo/cookie-header.png"
             alt=""
             height={100}
           />
           <div tw="flex justify-center items-center" style={{ gap: 20 }}>
-            <img
-              src={data.Socials.Social[0].profileImage}
-              alt=""
-              height={100}
-              width={100}
-              style={{ borderRadius: 50 }}
-            />
-            <div tw="text-white text-5xl" className=" text-5">
+            <div tw="text-white text-4xl" className=" text-5">
               {`@${data.Socials.Social[0].profileName}`}
             </div>
           </div>
         </div>
 
-        <div tw="flex flex-col gap-4" style={{ gap: 40 }}>
-          <div tw="flex text-white text-7xl items-center">
-            <span>This jar has</span>
-            <span tw="text-[#00D395] ml-2">100</span>
-            <span tw="ml-2">USDC</span>
+        <div tw="flex flex-col gap-4" style={{ gap: 50 }}>
+          <div tw="flex flex-col gap-4" style={{ gap: 10 }}>
+            <div tw="flex text-white text-7xl items-center">
+              <span>This jar has</span>
+              <span tw="text-[#00D395] ml-2">100</span>
+              <span tw="ml-2">USDC</span>
+            </div>
+            <div tw="flex text-white items-center text-4xl">
+              <span>You are allowed to withdraw upto</span>
+              <span tw="text-[#00D395] ml-2">12</span>
+              <span tw="ml-2">USDC</span>
+            </div>
           </div>
-          <div tw="flex text-white text-5xl items-center">
-            <span>You are allowed to withdraw upto</span>
-            <span tw="text-[#00D395] ml-2">12</span>
-            <span tw="ml-2">USDC</span>
+          <div tw="text-white text-4xl">
+            *this is based on your voting power :)
           </div>
-          <div tw="text-white">*this is based on your voting power :)</div>
         </div>
       </div>
     ),
     buttons: [
       <Button action="post" target="/worldcoin">
-        Verify with WorldId
+        Verify with Worldcoin
       </Button>,
     ],
   };
