e50d0fda19f583ab771125820227418a30bbdd01
Fabian Ferno <fabianferno@gmail.com>
2024-08-11 18:20:07 +0530

    Push notes page

---
+++ web/src/app/notes/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Notes from "@/components/screens/notes";
+
+export default function Page() {
+    return (
+        <div className="ring-1 ring-zinc-700 rounded-xl p-8 w-full">
+            <Notes />
+        </div>
+    );
+}
